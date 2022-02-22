0f87d0172a07977f417f0f50251ca6c15d0694d6
IAMZL177 <schukerleizhang@qq.com>
2022-02-22 09:51:42 +0800

    feat: gengxinapp

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
+    // 将腾讯发来的文件返回
     const echostr = req.url.split('&')[1].split('=')[1];
     res.end(echostr);
 });
